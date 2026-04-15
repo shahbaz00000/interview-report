d900cc85c8f68b0063a2784ec2c4bb5bb4ebf068
shahbaz00000 <kmdshahbaz98@gmail.com>
2026-04-15 10:52:37 -0700

    Update login API endpoint to use '/api/login'

--- frontend/src/services/loginServices.js
+++ frontend/src/services/loginServices.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const handleLoginApi = async (data) => {
     try {
         const response = await axios.post(
-            "https://interview-report-gf6x.onrender.com/login",
+            "https://interview-report-gf6x.onrender.com/api/login",
             data   // ✅ send directly
         );
         console.log("Login Response:", response.data);
